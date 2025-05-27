36d8006ad00e50ea13f0b54d0a9f20d00f96b52b
jerryskye <jarek@siedlarz.com>
2025-05-27 23:33:54 +0200

    Add even more years

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,12 @@ import { CalendarChinese } from "date-chinese";
 import ZodiacCalculator from "../ZodiacCalculator";
 
 const calculateYearTable = function () {
-  const cal = new CalendarChinese()
-  return [...Array(40).keys()].map((i) => {
-    const newYear = cal.newYear(1984 + i);
+  const cal = new CalendarChinese();
+  const currentYear = new Date().getFullYear();
+  const startYear = currentYear - 7;
+  const endYear = startYear - 69;
+  return [...Array(70).keys()].map((i) => {
+    const newYear = cal.newYear(endYear + i);
     cal.fromJDE(newYear);
     const date = cal.toDate();
     return <CalendarYear key={date} date={date} />;
